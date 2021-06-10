f89c7418bea6b6b765e017a118e60aaf27adff10
FlorianEifert <64579955+FlorianEifert@users.noreply.github.com>
2021-06-10 04:40:22 +0200

    Admin Table all User

---
+++ src/js/admin-controller-server.js
@@ -0,0 +1,73 @@
+const express = require('express');  
+const mysql = require('mysql'); 
+const cors = require('cors');
+const bcrypt = require("bcrypt"); 
+const parser = require("body-parser"); 
+const saltRounds = 10;
+
+const app = express();    
+
+app.use(express.json());  
+app.use(cors()); 
+app.use(express.urlencoded({ extended: false }));
+app.use(parser.urlencoded({ extended: false }));
+app.use(parser.json());
+
+const db = mysql.createConnection({
+host: "localhost", 
+user: "root", 
+password: "",
+database: "pwa",
+});
+
+
+
+
+app.listen(3001, () => console.log('listening on port 3001'));
+
+
+
+
+db.connect(function(error) { 
+    if (error) { 
+      console.log(error.msg);
+    } else {console.log('db ' + db.state)}
+  });
+  
+//get all users from database
+app.get('/user', (req, res) => {
+    let sql = 'SELECT `id_user`, `e_mail`, `upload_limit` FROM user';
+    let query = db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send(result);
+
+    });
+});
+  
+
+//get a user from database by id
+app.get('/user/:id_user', (req, res) => {
+    let sql = `SELECT id_user, e_mail, upload_limit FROM user WHERE id_user = ${req.params.id_user}`;
+    let query = db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send(result);
+    });
+});
+
+
+
+
+//update user Upload limit
+app.get('/updateuser/:id_user', (req, res) => {
+    let newUploadLimit = '55';
+    let sql = `UPDATE user SET upload_limit = '${newUploadLimit}' WHERE id_user = ${req.params.id_user}`;
+    let query = db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('user updated');
+    });
+});
+
+
