b84e2cc1cc8d47d74692521f0585e639ba79bd61
CalvinKluk <CalvinKluk@yahoo.de>
2021-06-12 23:45:26 +0200

    DOWNLOAD

--- src/public/static/js/admin-controller-server.js
+++ src/public/static/js/admin-controller-server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json());  
 app.use(cors());   
-app.use(express.static(__dirname + '/static'));
+app.use(express.static(__dirname + "/.."));
 app.use(express.urlencoded({ extended: false }))
 
 
